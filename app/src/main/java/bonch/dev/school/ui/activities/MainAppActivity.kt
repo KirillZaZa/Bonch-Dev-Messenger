0d0d2e3d6fafc125666353343082995d8bd94cdf
KirillZaZa <56443225+KirillZaZa@users.noreply.github.com>
2019-10-18 17:28:55 +0300

    Start!

---
+++ app/src/main/java/bonch/dev/school/ui/activities/MainAppActivity.kt
@@ -0,0 +1,14 @@
+package bonch.dev.school.ui.activities
+
+import bonch.dev.school.R
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainAppActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
